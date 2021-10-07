6e76dc082368a56fdedbce58ca89444d802c66aa
oluwaseun bello <bellomsean@gmail.com>
2021-10-07 19:34:16 +0100

    Heroku deploy

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,43 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      before_action :authorized, only: [:show]
+      # skip_before_action :verify_authenticity_token
+
+      def index 
+        @users = User.all
+        render json: @users
+      end
+
+      def show
+        render json: @current_user, status: 200
+      end
+
+      def create
+        @user = User.create(username: user_params[:username], password: user_params[:password])
+
+        if @user.valid?
+          token = encode_token({ user_id: @user.id })
+          render json: { status: :created, user: user_data(@user), token: token }, status: 201
+        else
+          render json: { errorMsgs: @user.errors.full_messages }, status: 422
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:username, :password, :user_id)
+      end
+
+      def user_data(user)
+        obj = {
+          id: user.id,
+          username: user.username,
+          admin: user.admin
+        }
+        obj
+      end
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  namespace 'api' do
+    namespace 'v1' do
+      resources :records, except: %i[new]
+      resources :items, except: %i[new]
+      resources :users, only: %i[show create index]
+      resources :login, only: %i[create]
+      resources :auto_login, only: %i[create]
+    end
+  end
+end
